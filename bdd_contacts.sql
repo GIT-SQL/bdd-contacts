0588a40d57d74d5eefaeacdf993b008f0a58a3b1
SQuaLe <visquert.lewis@gmail.com>
2023-11-23 09:34:23 +0100

    Création de la table 'contacts' avec ses champs.

--- bdd_contacts.sql
+++ bdd_contacts.sql
@@ -2,3 +2,15 @@ DROP DATABASE IF EXISTS bdd_contacts;
 CREATE DATABASE bdd_contacts CHARACTER SET = 'utf8' COLLATE = 'utf8_general_ci';
 
 USE bdd_contacts;
+
+CREATE TABLE contacts (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(70),
+    prenom VARCHAR(70),
+    date_de_naissance DATE,
+    sexe VARCHAR(10),
+    adresse TEXT,
+    cp VARCHAR(10),
+    ville VARCHAR(70),
+    pays_iso_3 VARCHAR(3)
+);
